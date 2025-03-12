85661753861aa71beea6df36fa0e39124c344735
Laura Jane Antunes <laura.antunes@gmail.com>
2025-03-11 21:52:38 -0300

    update expo, fix caminho

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AsyncStorage from './componentes/AsyncStorage';
+import AsyncStorage from './components/AsyncStorage';
 
 export default function App() {
   return (
